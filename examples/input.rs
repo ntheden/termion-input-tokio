3fff057ff1d1a1fe983a277b9da9eeaf93194383
Mark Rowe <mrowe@bdash.net.nz>
2021-01-04 14:01:11 -0800

    Fix up the example

--- examples/input.rs
+++ examples/input.rs
@@ -1,50 +1,38 @@
-extern crate futures;
-extern crate termion;
-extern crate termion_tokio;
-extern crate tokio;
-
 use std::io::stdout;
 use std::mem::drop;
 use std::process::exit;
 use std::time::Duration;
 
-use futures::{future::Either, lazy, Future, Stream};
-use tokio::{io::stdin, run, timer::Interval};
+use futures::{future::Either, StreamExt};
+use tokio::{io::stdin, time::interval};
 
 use termion::{event::Key, raw::IntoRawMode};
 use termion_tokio::input::TermReadAsync;
 
-fn main() {
-    run(lazy(|| {
-        let raw_term = stdout()
-            .into_raw_mode()
-            .map_err(|er| eprintln!("Error: {:?}", er))
-            .unwrap();
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let raw_term = stdout().into_raw_mode()?;
 
-        let input = stdin().keys_stream().map(Either::B).map_err(Either::B);
-        let ticks = Interval::new_interval(Duration::from_secs(3))
-            .map(Either::A)
-            .map_err(Either::A);
+    let input = stdin().keys_stream().map(Either::Right);
+    let ticks = interval(Duration::from_secs(3)).map(Either::Left);
 
-        let events = ticks.select(input);
+    let events = futures::stream::select(ticks, input);
 
-        events
-            .fold(raw_term, |raw_term, it| {
-                println!("Event: {:?}\r", it);
-                match it {
-                    Either::B(Key::Esc)
-                    | Either::B(Key::Char('q'))
-                    | Either::B(Key::Ctrl('c'))
-                    | Either::B(Key::Ctrl('b')) => {
-                        drop(raw_term);
-                        exit(0);
-                    }
-                    _ => (),
+    events
+        .fold(raw_term, |raw_term, it| {
+            println!("Event: {:?}\r", it);
+            match it {
+                Either::Right(Ok(Key::Esc))
+                | Either::Right(Ok(Key::Char('q')))
+                | Either::Right(Ok(Key::Ctrl('c')))
+                | Either::Right(Ok(Key::Ctrl('b'))) => {
+                    drop(raw_term);
+                    exit(0);
                 }
-                Ok(raw_term)
-            }).map(|_| ())
-            .map_err(|er| {
-                eprintln!("Error: {:?}", er);
-            })
-    }));
+                _ => (),
+            }
+            async { raw_term }
+        })
+        .await;
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,1 @@
-extern crate bytes;
-extern crate futures;
-extern crate termion;
-extern crate tokio;
-extern crate tokio_util;
-
 pub mod input;
